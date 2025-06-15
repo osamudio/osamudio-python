51166c90d3e6782f2558b48e7e781e1ac805ed75
osamudio <oscarsamudio@live.com>
2025-06-15 11:28:59 -0500

    Add files via upload

---
+++ 01-Programas-En-Python/00_clases/ modulo/paquete/mod_conversiones.py
@@ -0,0 +1,2 @@
+def convertir_binario(numero):
+    return print(f"El numero {numero} en binario es: {bin(numero)}")
---
+++ 01-Programas-En-Python/00_clases/ modulo/paquete/mod_matematicas.py
@@ -0,0 +1,3 @@
+def sumar_numeros(primer, segundo):
+    resultado = primer + segundo
+    return print(f"la suma de {primer} + {segundo} es: {resultado}")
